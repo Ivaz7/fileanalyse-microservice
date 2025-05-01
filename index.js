7a8b92d9151dd3a455bdd4e3690cf4f1e101339b
Ivaz <ivazreza@gmail.com>
2025-05-01 10:11:14 +0700

    fix: css directory

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use('/public', express.static('/public'));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
-app.get('/', (req, res) => {
+app.get('/style.css', (req, res) => {
   res.sendFile(__dirname + '/public/style.css');
 })
 
