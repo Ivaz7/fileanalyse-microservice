cb8ca4e830646f0d3e978e95868bc136e278cd09
Ivaz <ivazreza@gmail.com>
2025-05-01 10:17:51 +0700

    fix: vercel problem test fcc

--- index.js
+++ index.js
@@ -1,38 +1,14 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
-const multer = require('multer')
-const upload = multer()
-
-var app = express();
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const upload = multer();
+const app = express();
 
 app.use(cors());
-app.use('/public', express.static('/public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
-});
-app.get('/style.css', (req, res) => {
-  res.sendFile(__dirname + '/public/style.css');
-})
-
 
 app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
-  const {
-    originalname,
-    mimetype,
-    size,
-  } = req.file
-
-  res.json({
-    name: originalname,
-    type: mimetype,
-    size: size
-  })
-})
-
-
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  const { originalname, mimetype, size } = req.file;
+  res.json({ name: originalname, type: mimetype, size });
 });
+
+module.exports = app;
